b86bde5fe0a6ee36ad4d5abe4fe3b2347656d528
Pat Farrell <pfarrell@realgravity.com>
2012-12-16 22:31:52 -0800

    navigation and basic dispaly

--- comics.rb
+++ comics.rb
@@ -2,6 +2,15 @@ require 'sinatra'
 require 'date'
 require 'haml'
 
+$config = YAML.load_file 'config/config.yml'
+
+def get_files(search_path, dest_path)
+  ret=[]
+  Dir.foreach(search_path) do |f|
+    ret.push("#{dest_path}/#{f}") unless f == "." || f==".."
+  end
+  ret
+end
 
 get '/' do
   redirect "/#{Date.today.to_s}"
@@ -15,6 +24,8 @@ get '/:date' do
   p "#{params['date']}"
   @date = Date.parse(params['date'])
   @title = "Comics for #{@date.to_s}"
+  @files = get_files("#{$config["dest_dir"]}/#{@date.to_s}", @date.to_s)
+  p @files.inspect
   haml :index
 end
 
